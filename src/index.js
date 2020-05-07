10bc03f31c4cf0266424ee8d6a7cc28283be6410
akred <akred@live.com>
2020-05-07 17:55:37 +0200

    Step 2 : Add avatar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
