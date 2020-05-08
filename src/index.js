34f2a2359a006237386001d4e8447e4a0a98831f
Akred <akred@live.com>
2020-05-08 17:34:27 +0200

    Step 5 : Add new props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="John" />
-            <CommentDetail author="Jane" />
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 3:00PM"
+                comment={faker.lorem.words()}
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="John"
+                timeAgo="Yesterday at 6:00PM"
+                comment={faker.lorem.words()}
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="2 days ago at 1:00PM"
+                comment={faker.lorem.words()}
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
