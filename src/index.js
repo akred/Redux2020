87a59e4ab10b95f1a51b5792d2c528ab67f644ed
akred <akred@live.com>
2020-05-08 17:04:37 +0200

    Step 4 : Passing props

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <CommentDetail author="Sam" />
+            <CommentDetail author="John" />
+            <CommentDetail author="Jane" />
         </div>
     );
 };
