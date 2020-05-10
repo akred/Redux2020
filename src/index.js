eb98d615210869d05ef833efa6d8d496964c0af3
Akred <akred@live.com>
2020-05-10 11:58:49 +0200

    Step 8 : Wrap blog post into Approval cards

--- src/index.js
+++ src/index.js
@@ -7,25 +7,31 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 3:00PM"
-        comment={faker.lorem.words()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="John"
-        timeAgo="Yesterday at 6:00PM"
-        comment={faker.lorem.words()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="2 days ago at 1:00PM"
-        comment={faker.lorem.words()}
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 3:00PM"
+          comment={faker.lorem.words()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="John"
+          timeAgo="Yesterday at 6:00PM"
+          comment={faker.lorem.words()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="2 days ago at 1:00PM"
+          comment={faker.lorem.words()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
