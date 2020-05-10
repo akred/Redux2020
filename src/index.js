829d4630cf7d4f52cd73137c7eec7ee25ff4272d
Akred <akred@live.com>
2020-05-10 11:25:08 +0200

    Step 6 : Formatting

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 3:00PM"
-                comment={faker.lorem.words()}
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="John"
-                timeAgo="Yesterday at 6:00PM"
-                comment={faker.lorem.words()}
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="2 days ago at 1:00PM"
-                comment={faker.lorem.words()}
-                avatar={faker.image.avatar()}
-            />
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 3:00PM"
+        comment={faker.lorem.words()}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="John"
+        timeAgo="Yesterday at 6:00PM"
+        comment={faker.lorem.words()}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="2 days ago at 1:00PM"
+        comment={faker.lorem.words()}
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
